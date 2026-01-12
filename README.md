1c75608bff5392d89cadef73f05ba464c918a8eb
George Barkoulis <gb2793@columbia.edu>
2026-01-12 10:53:59 -0500

    update Readme

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 We introduce a learning algorithm to discover neural network parameterized cohesive laws using displacement fields. 
 
 ![Alt](Figures/model.png)
+
+## Setup & Examples 
+To run the examples, execute `python main.py --case ex1` for the first example and  `python main.py --case ex2` for the second.
+
+## Dependencies
+
+The following libraries are required: 
+
+| Package               | Version (>=) |
+|-----------------------|--------------|
+| numpy                 | 1.25.2       |
+| torch                 | 2.0.1        |
+| nvidia-warp           | 1.6.0        |
+
+## Citation
+TODO
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-
+import argparse
 import yaml
 import warp as wp 
 from src.MPM_inverse import MPM_program_inv
 
-with open('./input_files/ex1.yaml', 'r') as file:
+parser = argparse.ArgumentParser(description="Run example cases")
+parser.add_argument(
+    "--case",
+    choices=["ex1", "ex2"],
+    default="ex1"
+)
+args = parser.parse_args()
+
+with open(f'./input_files/{args.case}.yaml', 'r') as file:
     Inp_file = yaml.safe_load(file)
 
 wp.init()
@@ -12,7 +20,7 @@ if wp.is_cuda_available():
 else:
     device = 'cpu'
 
+print(f'Running:{args.case}')
 MPM_config = MPM_program_inv(**Inp_file,device=device)
 MPM_config.train()
-
 
